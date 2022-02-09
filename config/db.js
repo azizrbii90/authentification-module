0d5a9037ccf49823fc0de8038cadd47aae119c99
azizrbii90 <rbiiaziz900@gmail.com>
2022-02-09 16:27:16 +0100

    init & connection BD

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+let env = JSON.parse(JSON.stringify(require('../.env.json')));
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(env.DATABASE_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        })
+        console.log("Successfully connected to the database")
+    } catch (error) {
+        console.log('Could not connect to the database. Exiting now...', error);
+        console.log(error.reason);
+        process.exit();
+    }
+}
+
+module.exports = connectDB;
